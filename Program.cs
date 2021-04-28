14015a7301d1aeb836d952f9755efd773b1857b7
Breno Viana <bmfviana@gmail.com>
2021-04-28 10:07:13 -0300

    adds default project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace overlord_enemy_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
