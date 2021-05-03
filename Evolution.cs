09beff09b1b547ea1a95702e079d5a005782d90a
Breno Viana <bmfviana@gmail.com>
2021-05-03 16:17:21 -0300

    adds MAP-Elites population

--- Evolution.cs
+++ Evolution.cs
@@ -4,47 +4,46 @@ namespace OverlordEnemyGenerator
 {
     public class Evolution
     {
-        public static void Evolve(Parameters p)
-        {
+        public static Population Evolve(
+            Parameters p
+        ) {
             // Initialize the random generator
             Random rand = new Random(p.seed);
+            // Initialize the MAP-Elites population
+            Population population = new Population(
+                SearchSpace.AllBehaviorTypes().Length,
+                SearchSpace.AllWeaponTypes().Length
+            );
             // Generate initial population
             for (int i = 0; i < p.rIndividuals; i++)
             {
-                Individual ind = Individual.GetRandomIndividual(rand, p.space);
-                Console.WriteLine(ind.enemy.health);
-                Console.WriteLine(ind.enemy.strength);
-                Console.WriteLine(ind.enemy.attackSpeed);
-                Console.WriteLine(ind.enemy.movementType);
-                Console.WriteLine(ind.enemy.movementSpeed);
-                Console.WriteLine(ind.enemy.behaviorType);
-                Console.WriteLine(ind.enemy.activeTime);
-                Console.WriteLine(ind.enemy.restTime);
-                Console.WriteLine(ind.weapon.weaponType);
-                Console.WriteLine(ind.weapon.projectileType);
-                Console.WriteLine(ind.weapon.projectileSpeed);
-                Console.WriteLine();
+                // Generate a new random individual
+                Individual individual = Individual.GetRandom(rand, p.space);
+                // TODO: Calculates the individual fitness
+                // Place the new individual in the MAP-Elites
+                population.PlaceIndividual(individual);
             }
+            population.Debug();
             // Run the generations
             for (int i = 0; i < p.generations; i++)
             {
-                Console.WriteLine("Generation: " + i);
                 // Apply the evolutionary operators
                 if (p.crossover > rand.Next(100))
                 {
                     // TODO: Crossover
-                    Console.WriteLine("Crossover");
                     if (p.mutation > rand.Next(100))
                     {
                         // TODO: Mutation
-                        Console.WriteLine("Mutation");
                     }
-                } else {
+                }
+                else
+                {
                     // TODO: Mutation
-                    Console.WriteLine("Mutation");
                 }
                 // TODO: Place in MAP-Elites
             }
+            // Return the MAP-Elites population
+            return population;
         }
     }
 }
---
+++ Population.cs
@@ -0,0 +1,82 @@
+using System;
+
+namespace OverlordEnemyGenerator
+{
+    /// This struct represents a population of a MAP-Elites search approach.
+    ///
+    /// The MAP-Elites population is an N-dimensional array of individuals, 
+    /// where each matrix's ax corresponds to a different feature.
+    ///
+    /// This particular population is mapped into enemy's behavior and weapons.
+    /// Each Elite (or matrix cell) corresponds to a combination of different 
+    /// types of behaviors and weapons.
+    public struct Population
+    {
+        // The MAP-Elites dimension. The dimension is defined by the number of 
+        // behavior types multiplied by the number of weapon Types.
+        public (int behavior, int weapon) dimension { get; }
+        // The MAP-Elites map (a matrix of individuals).
+        public Individual[,] map { get; }
+
+        /// Population constructor.
+        public Population(
+            int numberOfBehaviorTypes,
+            int numbefOfWeaponTypes
+        ) {
+            this.dimension = (numberOfBehaviorTypes, numbefOfWeaponTypes);
+            this.map = new Individual[dimension.behavior, dimension.weapon];
+        }
+
+        /// Add an individual in the MAP-Elites population.
+        ///
+        /// First, we identify which Elite the individual is classified in. 
+        /// Then, if the corresponding Elite is empty, the individual is placed 
+        /// there. Otherwise, we compare the both old and new individuals, and 
+        /// the best individual is placed in the corresponding Elite.
+        public void PlaceIndividual(
+            Individual individual
+        ) {
+            // Calculate the individual slot (Elite)
+            int b = (int) individual.enemy.behaviorType;
+            int w = (int) individual.weapon.weaponType;
+            // Check if the new individual deserves to survive
+            if (map[b, w] is null || individual.fitness > map[b, w].fitness) {
+                map[b, w] = individual;
+            }
+        }
+
+        /// Print the individuals of the MAP-Elites population.
+        public void Debug()
+        {
+            for (int b = 0; b < dimension.behavior; b++)
+            {
+                for (int w = 0; w < dimension.weapon; w++)
+                {
+                    Console.WriteLine(
+                        "Elite " + ((BehaviorType) b) + "-" + ((WeaponType) w)
+                    );
+                    if (map[b, w] is null)
+                    {
+                        Console.WriteLine("   Empty");
+                    }
+                    else
+                    {
+                        Individual i = map[b, w];
+                        Console.WriteLine("   " + i.enemy.health);
+                        Console.WriteLine("   " + i.enemy.strength);
+                        Console.WriteLine("   " + i.enemy.attackSpeed);
+                        Console.WriteLine("   " + i.enemy.movementType);
+                        Console.WriteLine("   " + i.enemy.movementSpeed);
+                        Console.WriteLine("   " + i.enemy.behaviorType);
+                        Console.WriteLine("   " + i.enemy.activeTime);
+                        Console.WriteLine("   " + i.enemy.restTime);
+                        Console.WriteLine("   " + i.weapon.weaponType);
+                        Console.WriteLine("   " + i.weapon.projectileType);
+                        Console.WriteLine("   " + i.weapon.projectileSpeed);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
