4fe550a6d46a0d97e94d86d6c50a2ca052baa2b7
Breno Viana <bmfviana@gmail.com>
2021-05-04 16:19:32 -0300

    implements mutation operator

--- Operators.cs
+++ Operators.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using System.Collections.Generic;
 
 namespace OverlordEnemyGenerator
@@ -18,21 +19,21 @@ namespace OverlordEnemyGenerator
         /// available for the next selection.
         public static Individual[] Selection(
             int amount,
-            Population population,
+            Population pop,
             Random rand
         ) {
             // List of Elites' coordinates
-            List<Coordinate> cs = population.GetElitesCoordinates();
+            List<Coordinate> cs = pop.GetElitesCoordinates();
             // Select `amount` individuals
             Individual[] individuals = new Individual[amount];
             for (int i = 0; i < amount; i++)
             {
                 // Perform tournament selection with 3 competitors
                 (Coordinate coordinate, Individual individual) = Tournament(
-                    3,                        // Number of competitors
-                    population,               // Population reference
-                    new List<Coordinate>(cs), // List of valid coordinates
-                    rand                      // Random number generator
+                    3,   // Number of competitors
+                    pop, // Population reference
+                    cs,  // List of valid coordinates
+                    rand // Random number generator
                 );
                 // Select a new individual
                 individuals[i] = individual;
@@ -50,7 +51,7 @@ namespace OverlordEnemyGenerator
         /// process explained in `Selection` function.
         static (Coordinate, Individual) Tournament(
             int amount,
-            Population population,
+            Population pop,
             List<Coordinate> coordinates,
             Random rand
         ) {
@@ -65,7 +66,7 @@ namespace OverlordEnemyGenerator
                 // Get a random coordinate
                 (int x, int y) rc = Util.RandomFromList(acds, rand);
                 // Get the corresponding competitor
-                competitors[i] = population.map[rc.x, rc.y];
+                competitors[i] = pop.map[rc.x, rc.y];
                 cpcds[i] = rc;
                 // Remove competitors from available competitors
                 acds.Remove(rc);
@@ -86,22 +87,78 @@ namespace OverlordEnemyGenerator
         }
 
 
-        /// Reproduve a new individual by mutating a parent individual.
+        /// Reproduce a new individual by mutating a parent individual.
         public static Individual Mutate(
             Individual individual,
+            SearchSpace ss,
             Random rand
         ) {
-            return null;
+            // New individual
+            Individual mutant = individual.Clone();
+            // Apply mutation
+            switch (rand.Next(11))
+            {
+                // Enemy attributes
+                case 0:
+                    mutant.enemy.health
+                        = Util.RandomInt(ss.rHealth, rand);
+                    break;
+                case 1:
+                    mutant.enemy.strength
+                        = Util.RandomInt(ss.rStrength, rand);
+                    break;
+                case 2:
+                    mutant.enemy.attackSpeed
+                        = Util.RandomFloat(ss.rAttackSpeed, rand);
+                    break;
+                case 3:
+                    mutant.enemy.movementType
+                        = Util.RandomFromArray(ss.rMovementType, rand);
+                    break;
+                case 4:
+                    mutant.enemy.movementSpeed
+                        = Util.RandomFloat(ss.rMovementSpeed, rand);
+                    break;
+                case 5:
+                    mutant.enemy.behaviorType
+                        = Util.RandomFromArray(ss.rBehaviorType, rand);
+                    break;
+                case 6:
+                    mutant.enemy.activeTime
+                        = Util.RandomFloat(ss.rActiveTime, rand);
+                    break;
+                case 7:
+                    mutant.enemy.restTime
+                        = Util.RandomFloat(ss.rRestTime, rand);
+                    break;
+                // Weapon attributes
+                case 8:
+                    mutant.weapon.weaponType
+                        = Util.RandomFromArray(ss.rWeaponType, rand);
+                    break;
+                case 9:
+                    mutant.weapon.projectileType
+                        = Util.RandomFromArray(ss.rProjectileType, rand);
+                    break;
+                case 10:
+                    mutant.weapon.projectileSpeed
+                        = Util.RandomFloat(ss.rProjectileSpeed, rand);
+                    break;
+            }
+            // Calculate new individual fitness
+            mutant.fitness = Fitness.Calculate(mutant);
+            // Return the new mutated individual
+            return mutant;
         }
 
 
         /// Reproduce two new individuals by appling BLX-alpha crossover.
-        public static (Individual, Individual) Crossover(
+        public static Individual[] Crossover(
             Individual individual1,
             Individual individual2,
             Random rand
         ) {
-            return (null, null);
+            return null;
         }
     }
 }
