bd67391f1ed02b0157fa07cb72cb7282963811cb
brenov <bmfviana@gmail.com>
2021-09-03 17:27:33 -0300

    updates the documentation of main file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/// This program is an Enemy Generator that evolves enemies via a MAP-Elites GA.
+/// The result of this program is a set of enemies written in JSON files.
+///
+/// ATTENTION: This program was designed for the game prototype of the Game
+///            Research Group of Universidade de São Paulo (USP). Therefore, the
+///            designed enemy representation and difficulty function may not
+///            work for other games.
+///
+/// This enemy generator receives five arguments:
+/// - a random seed;
+/// - the number of generations;
+/// - the initial population size;
+/// - the mutation chance, and;
+/// - the crossover chance.
+///
+/// Author: Breno M. F. Viana.
+
+using System;
 
 namespace EnemyGenerator
 {
@@ -10,12 +27,6 @@ namespace EnemyGenerator
         /// Error code for bad arguments.
         private const int ERROR_BAD_ARGUMENTS = 0xA0;
 
-        /// This enemy generator receives five arguments:
-        /// 0 - Random seed.
-        /// 1 - Number of generations.
-        /// 2 - Initial population size.
-        /// 3 - Mutation chance.
-        /// 4 - Crossover chance.
         static void Main(string[] args)
         {
             // Check if the expected number of parameters were inputted
@@ -27,20 +38,21 @@ namespace EnemyGenerator
 
             // Define the evolutionary parameters
             Parameters prs = new Parameters(
-                int.Parse(args[0]), // Random seed
-                int.Parse(args[1]), // Number of generations
-                int.Parse(args[2]), // Initial population size
-                int.Parse(args[3]), // Mutation chance
-                int.Parse(args[4])  // Crossover chance
+                int.Parse(args[0]),              // Random seed
+                int.Parse(args[1]),              // Number of generations
+                int.Parse(args[2]),              // Initial population size
+                int.Parse(args[3]),              // Mutation chance
+                int.Parse(args[4])               // Crossover chance
             );
 
             // Prepare the evolutionary process
             EnemyGenerator generator = new EnemyGenerator(prs);
 
-            // Generate a set of enemies
+            // Start the generative process and generate a set of enemies
             generator.Evolve();
 
-            // Write the collected data
+            // When the enemy generation process is over, then write the
+            // results and the collected data
             Output.WriteData(generator.GetData());
         }
     }
