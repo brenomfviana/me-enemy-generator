259cbedefbc5f1fb492059d1422124e132d65d21
brenov <bmfviana@gmail.com>
2021-09-04 16:05:25 -0300

    updates scripts

--- plot.py
+++ plot.py
@@ -7,19 +7,14 @@ import seaborn as sb
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 
-CHART_FOLDER = 'charts'
-
-# Check if the folder for charts exists and create it if it does not exist
-if not os.path.isdir(CHART_FOLDER):
-  os.mkdir(CHART_FOLDER)
-
 
 # List of indexes
-difficulty = ['8, 12', '12, 16', '16, 20', '20, 24', '24, 28']
+difficulty = ['8-12', '12-16', '16-20', '20-24', '24-28']
 # List of columns
 weapons = ['Barehand', 'Sword', 'Bow', 'Bomb Thrower', 'Shield', 'Cure Spell']
 
 
+# Convert the array into a map
 def to_map(array, attribute):
   shape = (len(difficulty), (len(weapons)))
   map = np.zeros(shape)
@@ -27,13 +22,14 @@ def to_map(array, attribute):
   for b in range(len(difficulty)):
     for w in range(len(weapons)):
       if array[i] is None:
-        map[b, w] = np.nan
+        map[b, w] = 0
       else:
         map[b, w] = array[i][attribute]
       i += 1
   return map
 
 
+# Plot the heatmap
 def plot_heatmap(map, folder, filename, pop, max):
   df = DataFrame(map, index=difficulty, columns=weapons)
   color = sb.color_palette('viridis_r', as_cmap=True)
@@ -45,56 +41,50 @@ def plot_heatmap(map, folder, filename, pop, max):
   plt.close()
 
 
-def plot(data, filename, folder):
+# Plot all charts
+def plot_charts(data, filename, folder):
   # Parse file
   obj = json.loads(data)
 
+  # Create the folder for the given parameters
   filename = filename.replace('.json', '')
   folder = folder +  os.path.sep + filename
   if not os.path.isdir(folder):
     os.mkdir(folder)
 
   # Fitness heatmap of the populations
-  fit_map_initial = to_map(obj['initial'], 'fitness')
-  fit_map_intermediate = to_map(obj['intermediate'], 'fitness')
-  fit_map_final = to_map(obj['solution'], 'fitness')
+  attribute = 'fitness'
+  f_initial = to_map(obj['initial'], attribute)
+  f_intermediate = to_map(obj['intermediate'], attribute)
+  f_final = to_map(obj['final'], attribute)
 
-  plot_heatmap(fit_map_initial, folder, filename, 'fitness_initial', 2)
-  plot_heatmap(fit_map_intermediate, folder, filename, 'fitness_intermediate', 2)
-  plot_heatmap(fit_map_final, folder, filename, 'fitness_final', 2)
-
-  # # Generation heatmap of the populations
-  # gen_map_initial = to_map(obj['initial'], 'generation')
-  # gen_map_intermediate = to_map(obj['intermediate'], 'generation')
-  # gen_map_final = to_map(obj['solution'], 'generation')
-
-  # plot_heatmap(gen_map_initial, folder, filename, 'generation_initial', int(sys.argv[3]))
-  # plot_heatmap(gen_map_intermediate, folder, filename, 'generation_intermediate', int(sys.argv[3]))
-  # plot_heatmap(gen_map_final, folder, filename, 'generation_final', int(sys.argv[3]))
-
+  max = 2
+  plot_heatmap(f_initial, folder, filename, 'fitness_initial', max)
+  plot_heatmap(f_intermediate, folder, filename, 'fitness_intermediate', max)
+  plot_heatmap(f_final, folder, filename, 'fitness_final', max)
 
   # Print measurements
   measure = {}
 
   # Calculate mean fitness
-  measure['mean_fit_map_initial'] = np.mean(fit_map_initial)
-  measure['mean_fit_map_intermediate'] = np.mean(fit_map_intermediate)
-  measure['mean_fit_map_final'] = np.mean(fit_map_final)
+  measure['mean_f_initial'] = np.mean(f_initial)
+  measure['mean_f_intermediate'] = np.mean(f_intermediate)
+  measure['mean_f_final'] = np.mean(f_final)
 
   # Calculate max fitness
-  measure['max_fit_map_initial'] = np.max(fit_map_initial)
-  measure['max_fit_map_intermediate'] = np.max(fit_map_intermediate)
-  measure['max_fit_map_final'] = np.max(fit_map_final)
+  measure['max_f_initial'] = np.max(f_initial)
+  measure['max_f_intermediate'] = np.max(f_intermediate)
+  measure['max_f_final'] = np.max(f_final)
 
   # Calculate min fitness
-  measure['min_fit_map_initial'] = np.min(fit_map_initial)
-  measure['min_fit_map_intermediate'] = np.min(fit_map_intermediate)
-  measure['min_fit_map_final'] = np.min(fit_map_final)
+  measure['min_f_initial'] = np.min(f_initial)
+  measure['min_f_intermediate'] = np.min(f_intermediate)
+  measure['min_f_final'] = np.min(f_final)
 
   # Calculate the standard deviation
-  measure['std_fit_map_initial'] = np.std(fit_map_initial)
-  measure['std_fit_map_intermediate'] = np.std(fit_map_intermediate)
-  measure['std_fit_map_final'] = np.std(fit_map_final)
+  measure['std_f_initial'] = np.std(f_initial)
+  measure['std_f_intermediate'] = np.std(f_intermediate)
+  measure['std_f_final'] = np.std(f_final)
 
   # Serializing json
   json_object = json.dumps(measure, indent = 4)
@@ -107,13 +97,21 @@ def plot(data, filename, folder):
     outfile.write(json_object)
 
 
+# Target folder to save the charts
+CHART_FOLDER = 'charts'
+
+# Check if the folder for charts exists and create it if it does not exist
+if not os.path.isdir(CHART_FOLDER):
+  os.mkdir(CHART_FOLDER)
+
+
 # Calculate the filename
 folder = 'results' + os.path.sep
-param_folder = sys.argv[2] + '-' # Number of generations
-param_folder += sys.argv[3]      # Initial population size
+param_folder = sys.argv[1] + '-' # Number of generations
+param_folder += sys.argv[2]      # Initial population size
 
 
-# Read all files form this folder
+# Read all the JSON files for the given parameters
 files = []
 filenames = []
 for p in Path(folder + param_folder).glob('*.json'):
@@ -121,9 +119,13 @@ for p in Path(folder + param_folder).glob('*.json'):
     files.append(f.read())
     filenames.append(p.name)
 
+
+# Create the folder for the given parameters
 folder = CHART_FOLDER + os.path.sep + param_folder
 if not os.path.isdir(folder):
   os.mkdir(folder)
 
+
+# Plot all the charts
 for i in range(len(files)):
-  plot(files[i], filenames[i], folder)
+  plot_charts(files[i], filenames[i], folder)
--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import random
 import numpy as np
 
 
+
 # --- Initialization
 
 # Initialize the random seed
@@ -11,6 +12,7 @@ random.seed(0)
 
 # Define the number of executions of each set of parameters
 executions = range(1)
+
 
 
 # --- Define the set of parameters
@@ -28,7 +30,22 @@ mutations = [30]
 crossovers = [80]
 
 
+
 # --- Perform experiment
+
+# Choose the executable
+if platform.system() == 'Linux':
+  executable = './bin/Debug/net5.0/publish/EnemyGenerator '
+elif platform.system() == 'Windows':
+  executable = 'start bin\\Debug\\net5.0\\publish\\EnemyGenerator '
+else:
+  print('This script is not able to run in this OS.')
+  exit()
+
+
+# Compile project
+os.system('dotnet publish')
+
 
 def run(g, p, m, c):
   # Generate a random seed
@@ -38,36 +55,54 @@ def run(g, p, m, c):
   for i in [rs, g, p, m, c]:
     parameters += str(i) + ' '
   # Print parameters
-  print(parameters)
+  print('Parameters=[', parameters, ']')
   # Run algoritm for the current set of parameters
   os.system(executable + parameters)
+
+
+def plot(g, p):
   # Plot the charts for the current set of parameters
+  parameters = ''
+  for i in [g, p]:
+    parameters += str(i) + ' '
   os.system('python plot.py ' + parameters)
 
-# Compile the code
-os.system('dotnet publish')
 
-# Executable location
-if platform.system() == 'Linux':
-  executable = './bin/Debug/net5.0/publish/EnemyGenerator '
-elif platform.system() == 'Windows':
-  executable = 'start bin\\Debug\\net5.0\\publish\\EnemyGenerator '
-else:
-  print('This script is not able to run in this OS.')
-  exit()
-
-# Variables for progress
-total = len(generations) * len(populations) * len(mutations) * len(crossovers) * len(executions)
+# Variables to control the experiment progress
+total = len(generations) * \
+  len(populations) * \
+  len(mutations) * \
+  len(crossovers) * \
+  len(executions)
 i = 1
 
 # Run the algorithm for all sets of parameters
+print('Running')
 for g in generations:
   for p in populations:
     for m in mutations:
       for c in crossovers:
-        for _ in executions:
+        for e in executions:
           # Run execuble
           run(g, p, m, c)
           # Print progress
           print("%.2f" % ((i / total) * 100))
           i += 1
+
+
+# Variables to control the experiment progress
+total = len(generations) * \
+  len(populations) * \
+  len(mutations) * \
+  len(crossovers)
+i = 1
+
+# Plot all the results
+print('Plotting')
+for g in generations:
+  for p in populations:
+    for m in mutations:
+      for c in crossovers:
+        # Print progress
+        print("%.2f" % ((i / total) * 100))
+        plot(g, p)
